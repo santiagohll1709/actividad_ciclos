1ff4ff4399ab35ae8ffec3c2e3dcb4a9b50a25fe
santiagohll1709 <santiagohll1709@gmail.com>
2016-11-01 21:53:30 -0500

    Create ejer_2_a.m

---
+++ ejer_2_a.m
@@ -0,0 +1,15 @@
+clc 
+clear all
+close all
+nt=10000.001;
+for n=1:nt
+    ValorP(n)=(1/((2*n-1)));
+    if sum(n==[1:2:nt])==1
+        ValorP(n)=ValorP(n);
+    
+    else
+        2
+        ValorP(n)=-ValorP(n);
+    end 
+end
+sum(ValorP)*4
