07a613798d7ee61368c2fef9faa107add990a988
santiagohll1709 <santiagohll1709@gmail.com>
2016-11-11 17:41:11 -0500

    Create ejer_17.m

---
+++ ejer_17.m
@@ -0,0 +1,17 @@
+clc
+clear all 
+close all 
+t=(0:0.1:10);
+vs=3*exp(-t/3).*sin(pi.*t)
+for i=1:length(vs)
+    if vs(i)>0
+        vl=vs
+    end 
+    if vs(i)==0
+        vl=0
+    end
+
+end
+figure
+plot(vl,t)
+%profe asi  queda la grafica o estoy grave
